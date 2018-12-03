3c90a1692925d5c6592600b2c1db3043cd80efc9
wxx <921623221@qq.com>
2018-12-03 20:05:03 +0800

    漏传的ManagerRoleService.java

---
+++ src/src/main/java/com/gxu/tbvp/service/ManagerRoleService.java
@@ -0,0 +1,8 @@
+package com.gxu.tbvp.service;
+
+
+import com.gxu.tbvp.domain.ManagerRole;
+
+public interface ManagerRoleService extends IService<ManagerRole> {
+    public void addUserRole(ManagerRole managerRole);
+}
