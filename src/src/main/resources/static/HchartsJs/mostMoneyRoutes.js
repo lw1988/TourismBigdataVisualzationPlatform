922b41ed4e329db60a926e082854df1cd0e631eb
wxx <921623221@qq.com>
2018-12-03 21:16:31 +0800

    组合路线修改

--- src/src/main/resources/static/HchartsJs/mostMoneyRoutes.js
+++ src/src/main/resources/static/HchartsJs/mostMoneyRoutes.js
@@ -111,4 +111,232 @@ $.getJSON('HchartsJs/data/000001.json', function (data){
             }
         }]
     });
+    var chart = Highcharts.stockChart('container_1', {
+        rangeSelector: {
+            selected: 1,
+            inputDateFormat: '%Y-%m-%d'
+        },
+        title: {
+            text: '旅游路线历史盈利'
+        },
+        xAxis: {
+            dateTimeLabelFormats: {
+                millisecond: '%H:%M:%S.%L',
+                second: '%H:%M:%S',
+                minute: '%H:%M',
+                hour: '%H:%M',
+                day: '%m-%d',
+                week: '%m-%d',
+                month: '%y-%m',
+                year: '%Y'
+            }
+        },
+        tooltip: {
+            split: false,
+            shared: true,
+        },
+        yAxis: [{
+            labels: {
+                align: 'right',
+                x: -3
+            },
+            title: {
+                text: '盈利'
+            },
+            height: '65%',
+            resize: {
+                enabled: true
+            },
+            lineWidth: 2
+        }, {
+            labels: {
+                align: 'right',
+                x: -3
+            },
+            title: {
+                text: '成交量'
+            },
+            top: '65%',
+            height: '35%',
+            offset: 0,
+            lineWidth: 2
+        }],
+        series: [{
+            type: 'candlestick',
+            name: '旅游路线',
+            color: 'green',
+            lineColor: 'green',
+            upColor: 'red',
+            upLineColor: 'red',
+            tooltip: {
+            },
+            navigatorOptions: {
+                color: Highcharts.getOptions().colors[0]
+            },
+            data: ohlc,
+            dataGrouping: {
+                units: groupingUnits
+            },
+            id: 'sz'
+        },{
+            type: 'column',
+            data: volume,
+            yAxis: 1,
+            dataGrouping: {
+                units: groupingUnits
+            }
+        }]
+    });
+    var chart = Highcharts.stockChart('container_2', {
+        rangeSelector: {
+            selected: 1,
+            inputDateFormat: '%Y-%m-%d'
+        },
+        title: {
+            text: '旅游路线历史盈利'
+        },
+        xAxis: {
+            dateTimeLabelFormats: {
+                millisecond: '%H:%M:%S.%L',
+                second: '%H:%M:%S',
+                minute: '%H:%M',
+                hour: '%H:%M',
+                day: '%m-%d',
+                week: '%m-%d',
+                month: '%y-%m',
+                year: '%Y'
+            }
+        },
+        tooltip: {
+            split: false,
+            shared: true,
+        },
+        yAxis: [{
+            labels: {
+                align: 'right',
+                x: -3
+            },
+            title: {
+                text: '盈利'
+            },
+            height: '65%',
+            resize: {
+                enabled: true
+            },
+            lineWidth: 2
+        }, {
+            labels: {
+                align: 'right',
+                x: -3
+            },
+            title: {
+                text: '成交量'
+            },
+            top: '65%',
+            height: '35%',
+            offset: 0,
+            lineWidth: 2
+        }],
+        series: [{
+            type: 'candlestick',
+            name: '旅游路线',
+            color: 'green',
+            lineColor: 'green',
+            upColor: 'red',
+            upLineColor: 'red',
+            tooltip: {
+            },
+            navigatorOptions: {
+                color: Highcharts.getOptions().colors[0]
+            },
+            data: ohlc,
+            dataGrouping: {
+                units: groupingUnits
+            },
+            id: 'sz'
+        },{
+            type: 'column',
+            data: volume,
+            yAxis: 1,
+            dataGrouping: {
+                units: groupingUnits
+            }
+        }]
+    });
+    var chart = Highcharts.stockChart('container_3', {
+        rangeSelector: {
+            selected: 1,
+            inputDateFormat: '%Y-%m-%d'
+        },
+        title: {
+            text: '旅游路线历史盈利'
+        },
+        xAxis: {
+            dateTimeLabelFormats: {
+                millisecond: '%H:%M:%S.%L',
+                second: '%H:%M:%S',
+                minute: '%H:%M',
+                hour: '%H:%M',
+                day: '%m-%d',
+                week: '%m-%d',
+                month: '%y-%m',
+                year: '%Y'
+            }
+        },
+        tooltip: {
+            split: false,
+            shared: true,
+        },
+        yAxis: [{
+            labels: {
+                align: 'right',
+                x: -3
+            },
+            title: {
+                text: '盈利'
+            },
+            height: '65%',
+            resize: {
+                enabled: true
+            },
+            lineWidth: 2
+        }, {
+            labels: {
+                align: 'right',
+                x: -3
+            },
+            title: {
+                text: '成交量'
+            },
+            top: '65%',
+            height: '35%',
+            offset: 0,
+            lineWidth: 2
+        }],
+        series: [{
+            type: 'candlestick',
+            name: '旅游路线',
+            color: 'green',
+            lineColor: 'green',
+            upColor: 'red',
+            upLineColor: 'red',
+            tooltip: {
+            },
+            navigatorOptions: {
+                color: Highcharts.getOptions().colors[0]
+            },
+            data: ohlc,
+            dataGrouping: {
+                units: groupingUnits
+            },
+            id: 'sz'
+        },{
+            type: 'column',
+            data: volume,
+            yAxis: 1,
+            dataGrouping: {
+                units: groupingUnits
+            }
+        }]
+    });
 });
