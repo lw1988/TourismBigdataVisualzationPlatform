59a8989ce9b7de25a435e2b2859684cf86edfb95
liangbirui <liangbirui@foxmail.com>
2019-01-02 09:34:26 +0800

    修改需求图谱

--- src/src/main/resources/static/HchartsJs/wordcount.js
+++ src/src/main/resources/static/HchartsJs/wordcount.js
@@ -18,7 +18,7 @@ $(function () {
             return arr;
         }, []);
     /* alert(JSON.stringify(data)); */
-    data=[{"name":"西湖","weight":13},{"name":"雷峰塔","weight":8},{"name":"浙江大学","weight":9},{"name":"灵隐寺","weight":6},{"name":"飞来峰","weight":3},{"name":"青芝坞","weight":4},{"name":"西溪国家湿地公园","weight":5},{"name":"曲院风荷","weight":3},{"name":"断桥残雪","weight":7},{"name":"白堤","weight":5},{"name":"孤山","weight":5},{"name":"钱塘江大桥","weight":10}]
+    data=[{"name":"两江西湖","weight":13},{"name":"漓江","weight":8},{"name":"遇龙河","weight":9},{"name":"象山景区","weight":6},{"name":"银子岩","weight":3},{"name":"芦笛岩","weight":4},{"name":"世外桃源","weight":5},{"name":"龙脊梯田","weight":3},{"name":"十里画廊","weight":7},{"name":"叠彩山","weight":5},{"name":"月亮山","weight":5},{"name":"九马画山","weight":10}]
     Highcharts.chart('wordcount', {
         title: {
             text: null
