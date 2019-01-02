5397261f21025a714f7e6d964ab60707d0795660
liangbirui <liangbirui@foxmail.com>
2019-01-02 09:44:33 +0800

    修改图谱

--- src/src/main/resources/static/HchartsJs/wordcount.js
+++ src/src/main/resources/static/HchartsJs/wordcount.js
@@ -18,7 +18,7 @@ $(function () {
             return arr;
         }, []);
     /* alert(JSON.stringify(data)); */
-    data=[{"name":"两江西湖","weight":13},{"name":"漓江","weight":8},{"name":"遇龙河","weight":9},{"name":"象山景区","weight":6},{"name":"银子岩","weight":3},{"name":"芦笛岩","weight":4},{"name":"世外桃源","weight":5},{"name":"龙脊梯田","weight":3},{"name":"十里画廊","weight":7},{"name":"叠彩山","weight":5},{"name":"月亮山","weight":5},{"name":"九马画山","weight":10}]
+    data=[{"name":"两江西湖","weight":13},{"name":"漓江","weight":8},{"name":"日月塔","weight":9},{"name":"象山景区","weight":6},{"name":"银子岩","weight":3},{"name":"芦笛岩","weight":4},{"name":"世外桃源","weight":5},{"name":"龙脊梯田","weight":3},{"name":"十里画廊","weight":7},{"name":"叠彩山","weight":5},{"name":"月亮山","weight":5},{"name":"九马画山","weight":10}]
     Highcharts.chart('wordcount', {
         title: {
             text: null
