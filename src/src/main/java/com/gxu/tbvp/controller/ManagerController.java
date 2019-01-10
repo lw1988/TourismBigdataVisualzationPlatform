dc4cfa65c0081cf3b96251172937817d571dc311
guozhirou <631035080@qq.com>
2019-01-10 10:34:28 +0800

    补传

--- src/src/main/java/com/gxu/tbvp/controller/ManagerController.java
+++ src/src/main/java/com/gxu/tbvp/controller/ManagerController.java
@@ -21,7 +21,7 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
-@Controller
+@RestController
 @RequestMapping("/managers")
 public class ManagerController {
     @Resource
